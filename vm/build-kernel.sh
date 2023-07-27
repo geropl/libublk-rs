9140b44ccc9994c55c7f41a7b3f0315a36753bf7
csweichel <chris@gitpod.io>
2023-07-27 16:02:20 +0000

    Use defconfig/kvmguest kernel config

--- vm/build-kernel.sh
+++ vm/build-kernel.sh
@@ -10,7 +10,9 @@ sudo apt-get install -yq bc flex bison yacc libelf-dev
 curl -OL https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.41.tar.xz
 tar xvf linux-6.1.41.tar.xz
 cd linux-6.1.41
-cp "$ORGDIR/linux-config" .config
+make defconfig
+make kvm_guest.config
+sed -i /# CONFIG_BLK_DEV_UBLK is not set/CONFIG_BLK_DEV_UBLK=y/g
 make -j 4
 
 mkdir -p $ORGDIR/_output
